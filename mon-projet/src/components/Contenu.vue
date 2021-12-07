c274c6e7708eef8e082aa8c1dfd6db9d45f6c968
Yannick B <yannickbiheul@gmail.com>
2021-12-07 16:30:29 +0100

    Abandon du cours, passage à Vue.Js 3

---
+++ mon-projet/src/components/Contenu.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div class="container mt-5">
+        <liste :myArr="myArr" :txt="txt"></liste>
+        <liste :myArr="myArr" :txt="txt"></liste>
+    </div>
+
+</template>
+
+<script>
+
+    import Liste from './Liste/Liste.vue'
+
+    export default {
+        name: "Contenu",
+        // data() = data: function()...
+        data() {
+            return {
+                myArr: [
+                    {titre: 'Inception', date: 2010},
+                    {titre: 'Avatar', date: 2009},
+                    {titre: 'Seven', date: 1995}
+                ],
+                txt: 'Hello World'
+            }
+        },
+        components: {
+            'liste': Liste
+        },
+    }
+
+</script>
+
+<style>
+
+</style>
